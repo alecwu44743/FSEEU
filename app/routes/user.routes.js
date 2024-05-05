3eca4b90963a82e0afd25ecaad9e999a1fdc0768
Alec Wu <alec.wu44743@gmail.com>
2024-05-06 03:14:41 +0800

    refactor: Remove commented out code and unused routes in user.routes.js

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -2,19 +2,6 @@ const router = require("express").Router();
 const { authJwt } = require("../middleware");
 const userService = require("../services/user.service");
 
-// router.get("/usertest", (req, res) => {
-//     console.log("[>] [routes] /usertest");
-// 	res.send("userroutes test is successful");
-// });
-
-// router.post("/userposttest", 
-//     authJwt.verifyToken,
-//     (req, res) => {
-//     console.log("[>] [routes] /userposttest");
-//     const username = req.body.username;
-//     res.send("your username is: " + username)
-// })
-
 router.get("/test/all", userService.allAccess);
 
 router.get(
