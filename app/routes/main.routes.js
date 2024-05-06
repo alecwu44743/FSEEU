7d246515e8f9916e466b0c9b7b54576c10034a48
Alec Wu <alec.wu44743@gmail.com>
2024-05-06 11:22:17 +0800

    feat: Add authentication middleware to submit route in main.routes.js

--- app/routes/main.routes.js
+++ app/routes/main.routes.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 const mainService = require("../services/main.service");
+const { authJwt } = require("../middleware");
 
 router.get("/", mainService.home);
+
 router.get("/helloworld", mainService.helloworld);
+
+router.post(
+    "/submit", 
+    [authJwt.verifyToken],
+    mainService.submit
+);
 
 module.exports = router;
--- app/services/main.service.js
+++ app/services/main.service.js
@@ -1,3 +1,5 @@
+const { connectDatabase, getDatabase } = require("../models");
+
 const home = (req, res) => {
     console.log('[>] [services] Home :)');
     res.send('轟姆，噗呲噗呲，咚噠噠噠');
@@ -8,9 +10,36 @@ const helloworld = (req, res) => {
     res.send('Hello World，噗呲噗呲，咚噠噠噠');
 }
 
+const submit = (req, res) => {
+    console.log('[>] [services] Submit :)');
+    
+    const database = getDatabase();
+    const post_collection = database.collection(process.env.DB_COLLECTION_POST);
+
+    const post = {
+        course: req.body.course,
+        college: req.body.college,
+        teacher: req.body.teacher,
+        title: req.body.title,
+        content: req.body.content,
+        date: new Date().toISOString()
+    };
+
+    post_collection.insertOne(post, (err, user) => {
+        if (err) {
+            console.error('[x] Error inserting documents:', err);
+            res.status(500).send({ message: 'Error inserting documents: ' + err });
+        } else {
+            console.log(`[v] [${req.body.title}]'s documents added successfully`);
+            res.send({ message: `[${req.body.title}]'s documents added successfully` });
+        }
+    });
+}
+
 const mainService = {
     home: home,
-    helloworld: helloworld
+    helloworld: helloworld,
+    submit: submit
 };
 
 module.exports = mainService;
