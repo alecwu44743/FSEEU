d5ee59eaec70ab41dee5399fb7f04c33fd29f47b
Alec Wu <alec.wu44743@gmail.com>
2024-05-06 10:27:29 +0800

    feat: Add home route to main.routes.js

--- app/routes/main.routes.js
+++ app/routes/main.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const mainService = require("../services/main.service");
 
+router.get("/", mainService.home);
 router.get("/helloworld", mainService.helloworld);
 
 module.exports = router;
--- app/services/main.service.js
+++ app/services/main.service.js
@@ -1,8 +1,15 @@
+const home = (req, res) => {
+    console.log('[>] [services] Home :)');
+    res.send('轟姆，噗呲噗呲，咚噠噠噠');
+}
+
 const helloworld = (req, res) => {
+    console.log('[>] [services] Hello World :)');
     res.send('Hello World，噗呲噗呲，咚噠噠噠');
 }
 
 const mainService = {
+    home: home,
     helloworld: helloworld
 };
 
