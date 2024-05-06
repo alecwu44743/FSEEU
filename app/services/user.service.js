912d3f183982ed8f14355c0e4ab0e349ad266af5
Alec Wu <alec.wu44743@gmail.com>
2024-05-06 10:38:43 +0800

    feat: add logging statements to user.service.js

--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,16 +1,20 @@
 const allAccess = (req, res) => {
+    console.log("[>] [services] All Access :)");
     res.status(200).send("Public Content.");
 };
 
 const userBoard = (req, res) => {
+    console.log("[>] [services] User Board :)");
     res.status(200).send("User Content.");
 };
 
 const userplusBoard = (req, res) => {
+    console.log("[>] [services] User+ Board :)");
     res.status(200).send("User+ Content.");
 };
 
 const moderatorBoard = (req, res) => {
+    console.log("[>] [services] Moderator Board :)");
     res.status(200).send("Moderator Content.");
 };
 
