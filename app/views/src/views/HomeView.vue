4fb38c523c28790e851b430d94b6949b9a426f23
Hans <a0903150698@gmail.com>
2024-05-07 16:42:42 +0800

    Fetch text from backend API

---
+++ app/views/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>{{ text }}</p>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+
+const API_URL = "http://localhost:5038";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      text: "",
+    }
+  },
+  methods: {
+    async refreshData() {
+      axios.get(API_URL+"/")
+      .then((response)=>{
+        this.text=response.data;
+      })
+    }
+  },
+  mounted: function() {
+    this.refreshData();
+  }
+}
+</script>
