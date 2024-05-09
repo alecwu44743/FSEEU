4e901bac4ccafef006008036cf65568eefc4ded6
Hans <a0903150698@gmail.com>
2024-05-09 16:24:03 +0800

    update: signup

--- app/views/src/router/index.js
+++ app/views/src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import SignUp from '../views/SignUp.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
   },
   {
-    path: '/helloworld',
-    name: 'helloworld',
+    path: '/signin',
+    name: 'signin',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
---
+++ app/views/src/views/SignUp.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class= "signUp">
+        <form @submit.prevent="signUp">
+            <label>
+                帳號：<input type="text" name="username" v-model="username">
+            </label>
+            <br>
+            <label>
+                密碼：<input type="password" name="password" v-model="password">
+            </label>
+            <br>
+            <label>
+                Email：<input type="email" name="email" v-model="email">
+            </label>
+            <br>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+    <p>{{ username }}</p>
+    <p>{{ password }}</p>
+    <p>{{ email }}</p>
+</template>
+
+<script>
+import axios from 'axios'
+
+const API_URL = "http://localhost:8000";
+
+export default {
+    name: 'SignUp',
+    data() {
+        return {
+            username: "",
+            password: "",
+            email: "",
+        }
+    },
+    methods: {
+        signUp() {
+            // 構造要發送到後端的資料
+            const userData = {
+                username: this.username,
+                password: this.password,
+                email: this.email
+            };
+
+            // 使用 Axios 發送 POST 請求到後端的 API 端點
+            axios({
+                method: 'post',
+                url: API_URL+ 'api/auth/signup',
+                data: userData,
+            });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
