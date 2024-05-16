bd6ccbc420cd7a29bf1a8f714bf35e1af9200c6d
Hans <a0903150698@gmail.com>
2024-05-16 20:04:04 +0800

    feat: Add middleware for executing actions when navigating to the home page

--- app/routes/main.routes.js
+++ app/routes/main.routes.js
@@ -2,7 +2,11 @@ const router = require("express").Router();
 const mainService = require("../services/main.service");
 const { authJwt } = require("../middleware");
 
-router.get("/", mainService.home);
+router.get("/",
+[
+    authJwt.isUserAuthenticated,
+],
+mainService.home);
 
 router.get("/helloworld", mainService.helloworld);
 
