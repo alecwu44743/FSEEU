7a317842bd672a1e1461f9c8eb8eb78605f031af
Hans <a0903150698@gmail.com>
2024-05-17 21:06:19 +0800

    Refactor template structure for improved readability and organization

--- app/views/src/views/SignUp.vue
+++ app/views/src/views/SignUp.vue
@@ -1,43 +1,37 @@
 <template>
-    <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    </head>
-
-    <body>
-        <div class="container d-flex justify-content-center align-items-center" style="height: 80vh;">
-            <div class="row">
-                <div class="col-6">
-                    <div class="card" style="width: 25rem;">
-                        <div class="card-body">
-                            <form @submit.prevent="signUp" @input="check">
-                                <div class= "mb-3">
-                                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="學號" name="username" v-model="username" required="required">
-                                    <p v-if="usernameError" style="color: red;margin-top: 10px;">不符合學號格式！</p>
-                                </div>
-                                <div class= "mb-3">
-                                    <input type="password" class="form-control" id="formGroupExampleInput" placeholder="密碼"name="password" v-model="password" required="required">
-                                </div>
-                                <div class= "mb-3">
-                                    <input type="password" class="form-control" id="formGroupExampleInput" placeholder="密碼確認"name="password2" v-model="password2" required="required">
-                                    <p v-if="passwordError" style="color: red;margin-top: 10px;">密碼不相符！</p>
-                                </div>
-                                <div class= "mb-3">
-                                    <input type="email" class="form-control" id="formGroupExampleInput" placeholder="email" name="email" v-model="email" required="required">
-                                </div>
-                                <div class="separator"></div>
-                                <a href="/signin" style="text-decoration: none;color: black;margin-right: 280px;">返回登入</a>
-                                <br>
-                                <div class="d-grid gap-2">
-                                    <button type="submit" class="btn btn-light" style="margin-top: 20px;">註冊</button>
-                                </div>
-                            </form>
-                            <p style="margin-top: 20px;">{{ msg }}</p>
-                        </div>
+    <div class="container d-flex justify-content-center align-items-center" style="height: 80vh;">
+        <div class="row">
+            <div class="col-6">
+                <div class="card" style="width: 25rem;">
+                    <div class="card-body">
+                        <form @submit.prevent="signUp" @input="check">
+                            <div class= "mb-3">
+                                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="學號" name="username" v-model="username" required="required">
+                                <p v-if="usernameError" style="color: red;margin-top: 10px;">不符合學號格式！</p>
+                            </div>
+                            <div class= "mb-3">
+                                <input type="password" class="form-control" id="formGroupExampleInput" placeholder="密碼"name="password" v-model="password" required="required">
+                            </div>
+                            <div class= "mb-3">
+                                <input type="password" class="form-control" id="formGroupExampleInput" placeholder="密碼確認"name="password2" v-model="password2" required="required">
+                                <p v-if="passwordError" style="color: red;margin-top: 10px;">密碼不相符！</p>
+                            </div>
+                            <div class= "mb-3">
+                                <input type="email" class="form-control" id="formGroupExampleInput" placeholder="email" name="email" v-model="email" required="required">
+                            </div>
+                            <div class="separator"></div>
+                            <a href="/signin" style="text-decoration: none;color: black;margin-right: 280px;">返回登入</a>
+                            <br>
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn btn-light" style="margin-top: 20px;">註冊</button>
+                            </div>
+                        </form>
+                        <p style="margin-top: 20px;">{{ msg }}</p>
                     </div>
                 </div>
             </div>
         </div>
-    </body>
+    </div>
 </template>
 
 <script>
