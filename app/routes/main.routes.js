be5c3d1790461b5f209db72bc5f1c338d6607dec
Hans <a0903150698@gmail.com>
2024-05-17 21:09:26 +0800

    Remove middleware when router get to home

--- app/routes/main.routes.js
+++ app/routes/main.routes.js
@@ -2,11 +2,7 @@ const router = require("express").Router();
 const mainService = require("../services/main.service");
 const { authJwt } = require("../middleware");
 
-router.get("/",
-[
-    authJwt.isUserAuthenticated,
-],
-mainService.home);
+router.get("/", mainService.home);
 
 router.get("/helloworld", mainService.helloworld);
 
