390bbab83d33e782ef246aaaa0b948837a2d2655
Hans <a0903150698@gmail.com>
2024-05-20 21:47:33 +0800

    feat: add post urls in router

--- app/views/src/router/index.js
+++ app/views/src/router/index.js
@@ -3,6 +3,7 @@ import SignUp from '../views/SignUp.vue'
 import SignIn from '../views/SignIn.vue'
 import Home from '../views/Home.vue'
 import Submit from '../views/Submit.vue'
+import Post from '../views/Post.vue'
 
 const routes = [
   {
@@ -24,6 +25,12 @@ const routes = [
     path: '/submit',
     name: 'submit',
     component: Submit
+  },
+  {
+    path: '/post/:param',
+    name: 'post',
+    component: Post,
+    props: true
   }
 ]
 
