d613a3b878da3abcd60e136fa93e611901422c80
Hans <a0903150698@gmail.com>
2024-05-27 18:30:05 +0800

    feat: add feeds to get posts

--- app/views/src/views/Home.vue
+++ app/views/src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
-  <p>{{ text }}</p>
+  <div class= "container">
+    <div class="title-item" v-for= "item in items">
+      <a class="title-link" style="text-decoration: none; color: black;" :href= "`/post/${item.id}`">
+        <div class="title-and-date">
+          <span class="title">{{ item.title }}</span>
+          <span class="date">{{ item.date }}</span>
+        </div>
+      </a>
+    </div>
+  </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import axios from 'axios'
 
 const API_URL = "http://localhost:5038";
@@ -12,14 +20,18 @@ export default {
   name: 'Home',
   data() {
     return {
-      text: "",
+      items: []
     }
   },
   methods: {
     async refreshData() {
-      axios.get(API_URL+"/")
+      axios.get(API_URL+ "/feeds")
       .then((response)=>{
-        this.text=response.data;
+        this.items = response.data.map(item => ({
+          id: item._id,
+          title: item.title,
+          date: item.date.substring(0, 10)
+        }));
       })
     }
   },
@@ -28,3 +40,41 @@ export default {
   }
 }
 </script>
+
+<style>
+.container {
+  padding: 20px;
+}
+
+.title-item {
+  margin-bottom: 10px;
+  text-align: left; /* 確保所有標題文字置左 */
+  border-bottom: 1px solid #ddd; /* 添加分割線 */
+  padding-bottom: 10px; /* 分割線上方的間距 */
+}
+
+.title-link {
+  text-decoration: none;
+  color: black;
+  display: block;
+}
+
+.title-link:hover {
+  text-decoration: underline;
+}
+
+.title-and-date {
+  display: flex;
+  justify-content: space-between; /* 將內容置於彼此對齊的容器的兩側 */
+  align-items: end; /* 在容器中垂直居中對齊 */
+}
+
+.title {
+  font-size: 25px;
+}
+
+.date {
+  font-size: 15px;
+  float: right;
+}
+</style>
